e1163236c7ad66d3114cb5b88c28290958a718a4
sarahhhma <127348630+sarahhhma@users.noreply.github.com>
2023-05-31 23:29:18 -0700

    Update index.js
    Fixed an oversight in the count for player two.

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
 let oneEl = document.getElementById("p1-text")
 let twoEl = document.getElementById("p2-text")
-let count = 0
+let playerOneCount = 0
+let playerTwoCount = 0
 function addOnePOne() {
-    count += 1
-    oneEl.textContent = count
+    playerOneCount += 1
+    oneEl.textContent = playerOneCount
 }
 function addTwoPOne() {
-    count += 2
-    oneEl.textContent = count
+    playerOneCount += 2
+    oneEl.textContent = playerOneCount
 }
 function addThreePOne() {
-    count += 3
-    oneEl.textContent = count
+    playerOneCount += 3
+    oneEl.textContent = playerOneCount
 }
 function addOnePTwo() {
-    count += 1
-    twoEl.textContent = count
+    playerTwoCount += 1
+    twoEl.textContent = playerTwoCount
 }
 function addTwoPTwo() {
-    count += 2
-    twoEl.textContent = count
+    playerTwoCount += 2
+    twoEl.textContent = playerTwoCount
 }
 function addThreePTwo() {
-    count += 3
-    twoEl.textContent = count
+    playerTwoCount += 3
+    twoEl.textContent = playerTwoCount
 }
 function newGame() {
-    count = 0
+    playerOneCount = 0
+    playerTwoCount = 0
     oneEl.textContent = 0
     twoEl.textContent = 0
 }
